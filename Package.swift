98261c4ce06f60231800609e7d8f2078a6edaa96
Brian Boyle <brian.boyle@intercom.io>
2024-04-08 17:41:12 +0100

    Upating checksum for release 16.0.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "Intercom",
             url: "https://github.com/intercom/intercom-ios/releases/download/16.0.2/Intercom.xcframework.zip",
-            checksum: "e061f26872f833af04c0a7f442fa3a0cac8039a13ed96702652d4383273370b7"
+            checksum: "83f42253235f43ed9604e208a4117dd44f1a546dad34f4ad2bacf851ba68c514"
         )
     ]
 )
